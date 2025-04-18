686c3c4a401d31a7efa2b2c0108d7511c4981bea
Islamlogbibi <logbibimabrouk@gmail.com>
2025-04-18 15:06:37 +0100

    tp 03

---
+++ TP_03.java
@@ -0,0 +1,34 @@
+public class TP_03 {
+
+    
+    public static class Person {
+        String name;
+        String familyName;
+          public void displayFullname() {
+            System.out.println("FullName: " + name  +" "+familyName);
+        }
+         
+    }
+
+    
+    public static class Student extends Person {
+        int moyen;
+        public void displayMoyen() {
+            System.out.println("Moyen: " + moyen);
+        }
+        
+    
+    }
+
+   
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        student1.name = "mabrouk";
+        
+        student1.familyName = "logbibi";
+        student1.moyen = 20;
+
+        student1.displayFullname();
+        student1.displayMoyen();
+    }
+}
